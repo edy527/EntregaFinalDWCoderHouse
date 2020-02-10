2e8e397044afd7762801785d7826141ce4eeb84c
edy527 <edgardobeltran527@gmail.com>
2020-02-10 17:00:49 -0300

    enviador
    Se crea el archivo enviador PHP

--- enviar.php
+++ enviar.php
@@ -1,1 +1,14 @@
 <?php
+  $destino = "edgardobeltran527@gmail.com"
+  $email = $_POST["email"];
+  $nombre = $_POST["nombre"];
+  $apellido = $_POST["apellido"];
+  $mensaje = $_POST["mensaje"];
+  $motivo = $_POST["motivo"];
+
+  
+  $contenido = "E-mail: " . $email . "\n Nombre: " .$nombre . "\n Apellido: "  . $apellido "\n Mensaje: " . $mensaje . "\n Motivo: " .$motivo;
+
+  mail($destino, $motivo, $contenido);
+  header("Location: gracias.html ")
+?>
