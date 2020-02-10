2c22dd6a2830fc0b632f920b8f5a6f596fee3485
edy527 <edgardobeltran527@gmail.com>
2020-02-10 17:58:36 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,14 +1,18 @@
 <?php
-  $destino = "edgardobeltran527@gmail.com"
+  $destino = "edgardobeltran527@gmail.com";
   $email = $_POST["email"];
   $nombre = $_POST["nombre"];
   $apellido = $_POST["apellido"];
   $mensaje = $_POST["mensaje"];
-  $motivo = $_POST["motivo"];
+  /* $motivo = $_POST["motivo"]; */
 
   
-  $contenido = "E-mail: " . $email . "\n Nombre: " .$nombre . "\n Apellido: "  . $apellido "\n Mensaje: " . $mensaje . "\n Motivo: " .$motivo;
+  $contenido = "E-mail: " . $email . "\nNombre: " . $nombre . "\nApellido: " . $apellido "\nMensaje: " . $mensaje;
 
-  mail($destino, $motivo, $contenido);
-  header("Location: gracias.html ")
+  mail($destino, $contenido);
+  header("Location:gracias.html");
+
+
+
+  
 ?>
